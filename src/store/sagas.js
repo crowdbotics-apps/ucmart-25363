f53adb544ca58c319149ea4522e56d9f6da92ea5
Crowdbotics <team@crowdbotics.com>
2021-03-30 07:34:10 +0000

    Update store

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -2,15 +2,11 @@ import { put, call, all, spawn, takeEvery } from "redux-saga/effects"
 import { apiService } from "./services"
 import * as types from "./constants"
 import * as actions from "./actions"
-
 export default function* rootSaga() {
-  const sagas = [
-    // Example watcher
-    // getBalanceWatcher
-  ]
+  const sagas = []
   yield all(
     sagas.map(saga =>
-      spawn(function*() {
+      spawn(function* () {
         while (true) {
           try {
             yield call(saga)
